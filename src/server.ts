68497f34e7880b7143eecd962882ac92206c59a1
dspedroborges <xpedrostewart@gmail.com>
2025-10-22 18:59:28 -0300

    log middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user";
+import authRoutes from "./routes/auth";
+import { log } from "./middleware/log";
 
 dotenv.config();
 
@@ -9,8 +11,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(log);
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API running!");
