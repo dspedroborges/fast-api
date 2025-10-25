07ba3bca93f2900f7a3d7e1a7fc638258f79d8c7
dspedroborges <xpedrostewart@gmail.com>
2025-10-25 09:43:12 -0300

    a generic group of endpoints for an enitity, to simplify creation

---
+++ src/routes/entity.ts
@@ -0,0 +1,84 @@
+import { Router, Request, Response } from "express";
+import { prisma } from "../utils/prisma";
+import { authenticate, AuthenticatedRequest } from "../middleware/authenticate";
+
+const router = Router();
+
+router.get("/", authenticate, async (req: AuthenticatedRequest, res: Response) => {
+    if (!req.isAdmin) return res.status(403).send("Forbidden");
+
+    const page = parseInt(req.query.page as string) || 1;
+    const take = 10;
+    const skip = (page - 1) * take;
+
+    const [entities, total] = await Promise.all([
+        prisma.entities.findMany({
+            skip,
+            take,
+            orderBy: { createdAt: "desc" },
+        }),
+        prisma.entities.count(),
+    ]);
+
+    res.status(200).json({entities, total});
+});
+
+router.get("/:id", authenticate, async (req: AuthenticatedRequest, res: Response) => {
+    const entityId = Number(req.params.id);
+
+    const entity = await prisma.entities.findUnique({
+        where: {
+            id: entityId
+        }
+    });
+    res.status(201).json(entity);
+});
+
+router.post("/:id", authenticate, async (req: AuthenticatedRequest, res: Response) => {
+    const { name, description, price, stock } = req.body;
+
+    if (!req.isAdmin) return res.status(403);
+
+    await prisma.entities.create({
+        data: { name, description, price, stock },
+    });
+    res.status(201).json({ message: "entity created" });
+});
+
+router.put("/:id", authenticate, async (req: AuthenticatedRequest, res: Response) => {
+    const { name, description, price, stock } = req.body;
+    const entityId = Number(req.params.id);
+
+    if (!req.isAdmin) return res.status(403);
+
+    const entity = await prisma.entities.findUnique({
+        where: {
+            id: entityId
+        }
+    });
+
+    if (!entity) return;
+
+    await prisma.entities.update({
+        data: { name, description, price, stock },
+        where: {
+            id: entityId
+        }
+    });
+    res.status(201).json({ message: "entity updated" });
+});
+
+router.delete("/:id", async (req: AuthenticatedRequest, res: Response) => {
+    const entityId = Number(req.params.id);
+
+    if (!req.isAdmin) return res.status(403);
+
+    await prisma.entities.delete({
+        where: {
+            id: entityId
+        }
+    });
+    res.status(201).json({ message: "entity deleted" });
+});
+
+export default router;
